df6d0dca3c2e0c7f8c83f448ca24ab68b4b9c97e
QuanBamboo <ngocquannguyen94@gmail.com>
2024-08-15 20:14:11 +0700

    update

--- Module_1/Variable, Datatype & Operator/Ung_dung_chuyen_doi_tien_te.js
+++ Module_1/Variable, Datatype & Operator/Ung_dung_chuyen_doi_tien_te.js
@@ -1,10 +1,23 @@
+let amount = document.getElementById('amount').value;
+let currency = document.getElementById('currency').value;
+let toCurrency = document.getElementById('toCurrency').value;
+let result;
 
-
-
-
-
-
-function chuyenDoi(){
-
-    document.getElementById('amount').
+function changeMoney() {
+    if (currency == "VND" && toCurrency == "USD") {
+        result = 'Kết quả là' + amount / 25000 + '$';
+    } else if (currency == "USD" && toCurrency == "VND") {
+        result = 'Kết quả là: ' + amount * 25000 + 'VND';
+    } else if (currency == "USD" && toCurrency == "USD") {
+        result = 'Kết quả là: ' + amount + '$';
+    } else {
+        result = 'Kết quả là: ' + amount + 'VND';
+    }
+    document.getElementById('result').innerHTML = result;
 }
+
+
+
+
+
+
