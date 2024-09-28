a5714d18ca7c4cf1aa6d3d926a118e68b3c92d78
QuanBamboo <ngocquannguyen94@gmail.com>
2024-09-28 12:46:20 +0700

    update

---
+++ Module_2/index.java
@@ -0,0 +1,9 @@
+public class index {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        float c=19.6f;
+        double kq = (double) a / b;
+        System.out.println(kq);
+    }
+}
